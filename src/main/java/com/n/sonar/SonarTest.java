e5b0c9d411d58c2528835a09f2081ede77f8724b
n <n10101@yahoo.com>
2016-08-28 18:18:29 +0530

    code change

---
+++ src/main/java/com/n/sonar/SonarTest.java
@@ -0,0 +1,18 @@
+package com.n.sonar;
+
+public class SonarTest {
+
+	public static void main(String[] args) {
+		int i =10;
+		System.out.println("Hello World");
+		System.out.println(i);
+		
+		String s = "n";
+		for(int j=0; j<10; j++){
+			s = s+j;
+		}
+		System.out.println(s);
+
+	}
+
+}
